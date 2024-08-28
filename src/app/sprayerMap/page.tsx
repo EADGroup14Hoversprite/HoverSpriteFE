c48a09d906413587b30f523509f375f9d6e13682
Harry <tamkt2274@hotmail.com>
2024-08-29 01:47:46 +0700

    Implemented routing, POI searching, auto complete for searches, add locations, show directions

--- src/app/sprayerMap/page.tsx
+++ src/app/sprayerMap/page.tsx
@@ -1,20 +1,8 @@
-import dynamic from "next/dynamic";
-import { useMemo } from "react";
 
-export default async function Page() {
-    const Map = useMemo(() => dynamic(
-        () => import('@/components/map/'),
-        {
-            loading: () => <p>A map is loading</p>,
-            ssr: false
-        }
-    ), [])
+import MapComponent from "@/components/map/mapComponent";
 
-    return (
-        <>
-            <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
-                <Map posix={[10.7291501, 106.693238]} />
-            </div>
-        </>
-    )
+export default function Page() {
+  return (
+    <MapComponent/>
+  );
 }
