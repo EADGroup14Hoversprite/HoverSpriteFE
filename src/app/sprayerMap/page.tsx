475167f0998e5bc78306ba264452167e329e1e13
Harry <tamkt2274@hotmail.com>
2024-09-15 14:00:33 +0700

    Added basic address-lookup map + address lookup form

--- src/app/sprayerMap/page.tsx
+++ src/app/sprayerMap/page.tsx
@@ -1,8 +1,10 @@
 
-import MapComponent from "@/components/map/mapComponent";
+import RoutingMap from "@/components/map/RoutingMap";
+import GeoSearchMap from "@/components/map/geoSearchMap";
+import GeoSearchForm from "@/components/map/geoSearchForm";
 
 export default function Page() {
   return (
-    <MapComponent/> //this is to show as example. Should put it in a smaller container to display to the user
+    <RoutingMap/> //this is to show as example. Should put it in a smaller container to display to the user
   );
 }
