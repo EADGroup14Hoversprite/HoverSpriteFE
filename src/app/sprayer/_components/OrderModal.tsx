edbe126a9aee65d170c76827b91506328269f919
Harry <tamkt2274@hotmail.com>
2024-09-17 12:54:45 +0700

    QR

--- src/app/sprayer/_components/OrderModal.tsx
+++ src/app/sprayer/_components/OrderModal.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import { IOrder } from "@/models/Order";
 import API from "@/utils/axiosClient";
-import RoutingMap from "@/components/map/RoutingMap"; // Assuming your RoutingMap is reusable here.
+import RoutingMap from "@/components/map/RoutingMap";
 
 interface OrderModalProps {
   order: IOrder;
@@ -11,7 +11,6 @@ interface OrderModalProps {
 }
 
 const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
-
   const confirmPayment = async () => {
     try {
       await API.post<{ message: string }>(
@@ -27,18 +26,35 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
 
   const updateOrderStatus = async (status: string) => {
     try {
-      await API.post<{ message: string, order: IOrder }>(
+      await API.post<{ message: string; order: IOrder }>(
         `/order/${order.id}/update-status`,
-        {status}
+        { status }
       );
-        } catch (e) {
-          return {
-            message: "Failed to update order status",
-          };
-        }
-      onClose();
-    } 
-  
+    } catch (e) {
+      return {
+        message: "Failed to update order status",
+      };
+    }
+    onClose();
+  };
+
+  const [qrCode, setQrCode] = useState<string | null>(null);
+
+  const generateQR = async () => {
+    try {
+      const response = await API.get(`/order/${order.id}/generate-qr`, {
+        responseType: "arraybuffer", // This tells axios to treat the response as binary data
+      });
+
+      const qrCodeBlob = new Blob([response.data], { type: "image/png" });
+      const qrCodeUrl = URL.createObjectURL(qrCodeBlob);
+      setQrCode(qrCodeUrl); // Store the image URL in state
+    } catch (e) {
+      return {
+        message: "Failed to generate QR",
+      };
+    }
+  };
 
   return (
     <div
@@ -48,29 +64,35 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
       className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50"
     >
       <div className="relative w-full max-w-6xl h-auto bg-white rounded-lg shadow-lg">
-        {/* Close Button */}
         <div className="flex items-center justify-between p-4 border-b">
           <h3 className="text-lg font-semibold text-gray-900">Order Details</h3>
-          <button
-            type="button"
-            onClick={onClose}
-            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-2"
-          >
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+          <div className="flex space-x-2">
+            <Button
+              className="mr-10 bg-yellow-600 hover:bg-yellow-800 text-white font-bold py-2 px-4 rounded"
+              onClick={() => generateQR()}
+              text="Generate QR code"
+            />
+            <button
+              type="button"
+              onClick={onClose}
+              className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-2"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
 
         {/* Modal Body with Grid Layout */}
@@ -97,7 +119,6 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
               <div className="pb-2 border-b">
                 <h4 className="text-lg font-semibold text-gray-700 flex items-center">
                   <span className="material-icons mr-2">Order Info</span>
-                  
                 </h4>
                 <p className="text-sm">
                   <strong>Order ID:</strong> {order.id}
@@ -116,7 +137,13 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
                 </p>
                 <p className="text-sm">
                   <strong>Status:</strong>{" "}
-                  <span className={order.status === "ASSIGNED" ? "text-red-600" : "text-green-600"}>
+                  <span
+                    className={
+                      order.status === "ASSIGNED"
+                        ? "text-red-600"
+                        : "text-green-600"
+                    }
+                  >
                     {order.status}
                   </span>
                 </p>
@@ -125,7 +152,6 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
               <div className="pb-2">
                 <h4 className="text-lg font-semibold text-gray-700 flex items-center">
                   <span className="material-icons mr-2">Payment Info</span>
-                  
                 </h4>
                 <p className="text-sm">
                   <strong>Total Cost:</strong> ${order.totalCost}
@@ -135,7 +161,13 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
                 </p>
                 <p className="text-sm">
                   <strong>Payment Status:</strong>{" "}
-                  <span className={order.paymentStatus === true ? "text-green-600" : "text-red-600"}>
+                  <span
+                    className={
+                      order.paymentStatus === true
+                        ? "text-green-600"
+                        : "text-red-600"
+                    }
+                  >
                     {order.paymentStatus}
                   </span>
                 </p>
@@ -159,12 +191,21 @@ const OrderModal: React.FC<OrderModalProps> = ({ order, onClose }) => {
 
           {/* Map Section - Slightly Taller */}
           <div className="md:col-span-2 w-full h-auto bg-gray-200 rounded-lg overflow-hidden shadow-inner">
-            <div className="h-72"> {/* Slightly increased height */}
+            <div className="h-72">
+              {" "}
+              {/* Slightly increased height */}
               <RoutingMap />
             </div>
           </div>
         </div>
       </div>
+
+      
+      {qrCode && (
+      <div className="absolute inset-0 flex items-center justify-center z-60">
+        <img src={qrCode} alt="QR Code" className="w-64 h-64" />
+      </div>
+    )}
     </div>
   );
 };
